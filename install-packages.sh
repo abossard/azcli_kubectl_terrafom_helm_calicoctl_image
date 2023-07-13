78c97633e2f4849899f37faae7a2a4a876e73f3f
Andre Bossard <anbossar@microsoft.com>
2023-07-13 16:08:29 +0200

    added psql

--- install-packages.sh
+++ install-packages.sh
@@ -35,7 +35,7 @@ wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/ke
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 
 apt-get update
-apt-get install -y azure-cli terraform packer consul vault
+apt-get install -y azure-cli terraform packer consul vault postgresql-client
 az aks install-cli
 # Delete cached files we don't need anymore (note that if you're
 # using official Docker images for Debian or Ubuntu, this happens
